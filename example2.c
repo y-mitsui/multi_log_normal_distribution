af601047948d58ce67b7ce42b2b9e48eeeb33618
Y-Mitsui <dyfomg@gmail.com>
2014-12-11 23:47:21 +0900

    upload

--- example2.c
+++ example2.c
@@ -17,30 +17,48 @@ gsl_matrix *gsl_inverse(gsl_matrix *m){
 	gsl_permutation_free(p);
 	return r;
 }
-double m_log_normal_distribution(gsl_vector *data,gsl_vector *u,gsl_matrix *sigma){
-	int i,j;
-	gsl_matrix *mdata=gsl_matrix_alloc(data->size,1);
-	gsl_matrix_set_col(mdata,0,data);
-	gsl_matrix *tmp=gsl_matrix_alloc(sigma->size1,mdata->size2);
-	gsl_matrix *tmp2=gsl_matrix_alloc(1,u->size);
-	gsl_matrix *tmp3=gsl_matrix_alloc(sigma->size1,u->size);
-	gsl_blas_dgemm (CblasNoTrans, CblasNoTrans,1.0, sigma, mdata,0.0, tmp);
-	gsl_matrix_mul_constant(tmp,sqrt(2*M_PI));
-	for(i=0;i<data->size;i++){
-		gsl_vector_set(data,i,1/2*pow(log(gsl_vector_get(data,i))-gsl_vector_get(u,i),2));
-	}
-	gsl_matrix *mdata2=gsl_matrix_alloc(1,data->size);
-	gsl_matrix_set_row(mdata2,0,data);
-	gsl_matrix *r=gsl_inverse(sigma);
-	gsl_blas_dgemm (CblasNoTrans, CblasNoTrans,1.0,mdata2, r, 0.0, tmp2);
-	for(i=0;i<tmp2->size1;i++){
-		for(j=0;j<tmp2->size1;j++){
-			gsl_matrix_set(tmp2,i,j,exp(-gsl_matrix_get(tmp2,i,j)));
-		}
-	}
-	gsl_blas_dgemm (CblasNoTrans, CblasNoTrans,1.0,tmp, tmp2, 0.0, tmp3);
-	return gsl_matrix_get(tmp3,0,0);
+double gsl_det(gsl_matrix *m){
+	gsl_permutation * p = gsl_permutation_alloc (m->size1);
+	gsl_matrix *lu=gsl_matrix_clone(m);
+	int s=0;
+	gsl_linalg_LU_decomp (lu,p,&s);           // LU分解
+	double n = gsl_linalg_LU_det (lu,s);    // 行列式
+	gsl_matrix_free(lu);
+	gsl_permutation_free(p);
 	
+	return n;
+}
+double m_log_normal_distribution(gsl_vector *data,gsl_vector *u,gsl_matrix *sigma){
+	int i;
+	
+	/* 1/det(sigma) */
+	double sigmaDetInv=1.0/gsl_det(sigma);
+	
+	/* 1/(y_1*y_2...y_p) */
+	double dataMulInv=1.0;
+	for(i=0;i<data->size;i++){
+		dataMulInv*=gsl_vector_get(data,i);
+	}
+
+	double constant=pow(1.0/sqrt(2*M_PI),data->size);
+	
+	/* log(y)-u */
+	gsl_matrix *logDataSubUWidth=gsl_matrix_alloc(1,data->size);
+	for(i=0;i<data->size;i++){
+		gsl_matrix_set(logDataSubUWidth,0,i,log(gsl_vector_get(data,i))-gsl_vector_get(u,i));
+	}
+	/* (log(y)-u)*sigma^-1*/
+	gsl_matrix *sigmaInv=gsl_inverse(sigma);
+	gsl_matrix *dataSubUSigma=gsl_matrix_alloc(1,data->size);
+	gsl_blas_dgemm (CblasNoTrans, CblasNoTrans,1.0,logDataSubUWidth, sigmaInv, 0.0, dataSubUSigma);
+
+	/* mul log(y)-u*/
+	gsl_matrix *solv1=gsl_matrix_alloc(1,1);
+	gsl_blas_dgemm (CblasNoTrans, CblasTrans,1.0,dataSubUSigma, logDataSubUWidth, 0.0, solv1);
+	
+	double argExp=-1.0/2.0*gsl_matrix_get(solv1,0,0);
+
+	return constant*sigmaDetInv*dataMulInv*exp(argExp);
 }
 double log_normal_distribution(double x,double u,double sigma){
 	return 1.0/(sqrt(2*M_PI)*sigma*x)*exp(-pow(log(x)-u,2)/2*sigma*sigma);
@@ -52,14 +70,14 @@ int main(void){
 	gsl_vector_set(u,0,0);
 	gsl_vector_set(u,1,0);
 	gsl_matrix *sigma=gsl_matrix_alloc(2,2);
-	gsl_matrix_set(sigma,0,0,1);
-	gsl_matrix_set(sigma,0,1,0);
-	gsl_matrix_set(sigma,1,0,0);
-	gsl_matrix_set(sigma,1,1,1);
-	for(x=0.01;x<30.0;x+=0.5){
-		for(y=0.01;y<30.0;y+=0.5){
+	gsl_matrix_set(sigma,0,0,1.0);
+	gsl_matrix_set(sigma,0,1,0.5);
+	gsl_matrix_set(sigma,1,0,0.5);
+	gsl_matrix_set(sigma,1,1,1.0);
+	for(x=0.01;x<15.0;x+=0.2){
+		for(y=0.01;y<15.0;y+=0.2){
 			gsl_vector_set(data,0,x);
-			gsl_vector_set(data,1,x);
+			gsl_vector_set(data,1,y);
 			printf("%lf %lf %lf\n",x,y,m_log_normal_distribution(data,u,sigma));
 		}
 	}
